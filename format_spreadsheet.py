ad8a4122e635a0b2948b3dd4695948c9d0c437be
Maria <mashasalimova777@gmail.com>
2022-08-22 21:35:28 +0500

    исправлены баги

--- format_spreadsheet.py
+++ format_spreadsheet.py
@@ -1,10 +1,13 @@
 import openpyxl
+from pathlib import Path
 
 
 # TODO: графический интерфейс
 # TODO: проблема: видит файл только если файл в папке проекта
-def format_spreadsheet(file_name, new_file_name):
-    wb = openpyxl.load_workbook(file_name)
+def format_spreadsheet(file_path, new_file_name):
+    a = r'\t'
+    wb = openpyxl.load_workbook(file_path)
+
     ws = wb.active
 
     wb1 = openpyxl.Workbook()
@@ -12,11 +15,16 @@ def format_spreadsheet(file_name, new_file_name):
     ws1.title = ws.title
     model_line = ()
     for row in range(1, ws.max_row + 1):
-        current_line = ([ws.cell(row, i).value for i in range(1, ws.max_column + 1)])
-        if ws.cell(row, 1).value and ws.cell(row, 2).value and ws.cell(row, 3).value:
-            model_line = ([ws.cell(row, i).value for i in range(1, 4)])
+        current_line = [ws.cell(row, i).value for i in range(1, ws.max_column + 1)]
+        print(current_line)
+        if ws.cell(row, 1).value:
+            model_line = [ws.cell(row, i).value for i in range(1, 4)]
+            print(model_line)
             ws1.append(current_line)
         else:
-            line_to_write = model_line + current_line[3:]
+            line_to_write = tuple(model_line) + tuple(current_line[3:])
             ws1.append(line_to_write)
     wb1.save(new_file_name)
+
+
+format_spreadsheet(file_path=r'C:\Users\Маша\Desktop\январь_22.xlsx', new_file_name='test6.xlsx')
